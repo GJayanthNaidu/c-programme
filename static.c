7c1ff5095cf4b122ebf0994e7cbb89030b22684c
GJayanthNaidu <naidugonugutnlajayanth>
2023-07-28 18:21:13 +0530

    static auto

---
+++ static.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int a;
+	static int b;
+	printf("\n Default value of auto variable is : %d ",a);
+	printf("\n Default value of static is : %d \n ",a);
+}
